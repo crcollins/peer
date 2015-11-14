b3ed7ff5ec6275fa9c2455b873b20b79c396f5eb
Christopher Collins <crcollins@cmu.edu>
2015-11-13 22:48:44 -0500

    Add ability to view Reviews in the admin

--- peer/admin.py
+++ peer/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from models import Paper
+from models import Paper, Review
 
 class PaperAdmin(admin.ModelAdmin):
     date_hierarchy = "submitted"
     list_display = ("title", "author", "published")
 
+
+class ReviewAdmin(admin.ModelAdmin):
+    date_hierarchy = "submitted"
+    list_display = ("paper", "author", "decision")
+
+
 admin.site.register(Paper, PaperAdmin)
+admin.site.register(Review, ReviewAdmin)
