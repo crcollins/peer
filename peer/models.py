96eb053c57d5cc84a753517e639a420942b1fbf7
Christopher Collins <crcollins@cmu.edu>
2015-11-13 21:21:18 -0500

    Cleanup the paper status values Reviews
    This cleanup is to prep the addition of

--- peer/models.py
+++ peer/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from django.conf import settings
 
 
+PENDING = 0
+REJECTED = 1
+REVISION = 2
+ACCEPTED = 3
+
+DECISION_CHOICES = (
+    (REJECTED, "Rejected"),
+    (REVISION, "Revision"),
+    (ACCEPTED, "Accepted"),
+)
+STATUS_CHOICES = ((PENDING, "Pending"), ) + DECISION_CHOICES
+
+
 class Paper(models.Model):
-    PENDING = 0
-    REJECTED = 1
-    REVISION = 2
-    ACCEPTED = 3
-    STATUS_CHOICES = (
-        (PENDING, "Pending"),
-        (REJECTED, "Rejected"),
-        (REVISION, "Revision"),
-        (ACCEPTED, "Accepted"),
-    )
     title = models.CharField(max_length=255)
     abstract = models.TextField(max_length=1024)
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
@@ -22,5 +25,5 @@ class Paper(models.Model):
     status = models.IntegerField(choices=STATUS_CHOICES, default=PENDING)
 
     def is_public(self):
-        return self.status == Paper.ACCEPTED
+        return self.status == ACCEPTED
 
